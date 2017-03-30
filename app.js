3c8940489c75998db406aac621b265f362f6dc71
Peter Gribanov <wcode404@gmail.com>
2017-03-30 16:28:32 +0300

    save parameters in cookies

--- app.js
+++ app.js
@@ -6,21 +6,29 @@ $(function () {
     var collection = {};
 
     parameters.on('keyup', function () {
+        render();
+    }).each(function () {
+        collection[this.id] = $(this).val(Cookies.get(this.id));
+    });
+
+
+    render();
+
+    function render() {
         var data = {};
 
-        $.each(collection, function(param, el) {
+        $.each(collection, function(param) {
             if (param == 'github' && !collection[param].val()) {
                 data[param] = collection.composer.val();
             } else {
                 data[param] = collection[param].val();
             }
+
+            Cookies.set(param, collection[param].val());
         });
 
         var result = Mustache.render(template, data);
         markdown.val(result);
         preview.html(window.markdownit().render(result));
-
-    }).each(function () {
-        collection[this.id] = $(this);
-    });
+    }
 });
