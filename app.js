bff6267cc9e48e0cf8e5b892e966a5bebd396782
Peter Gribanov <wcode404@gmail.com>
2017-05-18 10:57:23 +0300

    correct apply default maxAge

--- app.js
+++ app.js
@@ -18,10 +18,21 @@ $(function () {
         var data = {};
 
         $.each(collection, function(param) {
-            if (param == 'github' && !collection[param].val()) {
-                data[param] = collection.composer.val();
-            } else {
-                data[param] = collection[param].val();
+            data[param] = collection[param].val();
+
+            switch (param) {
+                case 'github':
+                    if (!data[param]) {
+                        data[param] = collection.composer.val();
+                    }
+                    break;
+                case 'maxAge':
+                    if (!data[param]) {
+                        data[param] = 3600;
+                    } else {
+                        data[param] = parseInt(data[param]);
+                    }
+                    break;
             }
 
             Cookies.set(param, collection[param].val());
